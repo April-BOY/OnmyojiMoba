ea6c216c337d8e5509093652d6329ea627778bbf
April-BOY <2111965204@qq.com>
2018-10-23 20:26:51 +0800

    全新式神、周免式神、全部式神布局完成，全新皮肤底部的皮肤滚动选项卡完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,26 +1,55 @@
 Page({
   data:{
-    
+    piFuInfo:[],
+    currentId:"ea103f45-3382-4823-8a92-7e385db8af75"
   },
-  onLoad:function(){
+  onLoad:function(getPifuData){
+    var that = this;
     wx.request({
-      url: 'https://comp-sync.webapp.163.com/g78_hero/free_convey?callback=jQuery111308366921136572285_1540169781437&_=1540169781439',
+      // url: 'https://comp-sync.webapp.163.com/g78_hero/free_convey?callback=jQuery111308366921136572285_1540169781437&_=1540169781439',
+      url:'https://sixhorse.game.163.com/news/outer/newslist.do?callback=jQuery111305420124445720846_1540285706302&contentkind=29513&_=1540285706303',
       header:{
         'content-type': 'application/json',
         'content-type': 'application/x-www-form-urlencoded'
       },
       responseType:"text",
       success:function(res){
-        // console.log(res.data);
-        var start = res.data.indexOf("{");
+        /**
+         * 解决返回的数据是 Unicode 编码的问题
+         */
+        /* var start = res.data.indexOf("{");
         var end = res.data.lastIndexOf("}")+1;
-        var string = res.data.slice(start,end)
+        var string = res.data.slice(start,end);
         var json = JSON.parse(string)
         console.log(json.data);
-      }
-    })
+        var url = "https://ok.166.net/gameyw-gbox/moba/"; */
+        var pifuJson = that.unicodeToJson(res.data);
+        console.log(pifuJson.result);
+        that.setData({
+          piFuInfo:pifuJson.result
+        });
+        console.log(that.data.piFuInfo[0].comment);
+      },
+    });
   },
+//   getPifuData:function(){
+//     wx:request({
+//     	url:'https://sixhorse.game.163.com/news/outer/newslist.do?callback=jQuery111305420124445720846_1540285706302&contentkind=29513&_=1540285706303',
+//     	success:function(){
+//     		
+//     	}
+//     });
+//   },
   switchContent:function(e){
-    console.log(e.target)
+    this.setData({
+      currentId:e.currentTarget.dataset.id
+    });
+  },
+  unicodeToJson:function(obj){
+    var start = obj.indexOf("{");
+    var end = obj.lastIndexOf("}")+1;
+    var string = obj.slice(start,end);
+    var json = JSON.parse(string);
+    return json;
   }
 })
