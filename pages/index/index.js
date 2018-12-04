f9c820032aa3c5a1d391838936e4024423851afb
April-BOY <2111965204@qq.com>
2018-12-04 19:11:55 +0800

    Refactor: perfect index component with the interface's newest data

--- pages/index/index.js
+++ pages/index/index.js
@@ -18,15 +18,16 @@ Page({
     ],
     flag:false,
 		piFuInfo: [],
-    currentPifuUrl:"https://nie.res.netease.com/r/pic/20181019/e71936c2-8663-40b5-ba1f-4d6eaf1bf1dc.png",
-    currentTit:"新皮肤：妖狐 返校·情窦",
-    currentDesc:"2018.10.19上架服装铺",
-		currentId: "ea103f45-3382-4823-8a92-7e385db8af75",
+    currentPifuUrl:"",
+    currentTit:"",
+    currentDesc:"",
+		currentId: "",
     currentIndex:0,
     shiShenLuInfo:[],
+    // 获取式神录中图片的接口
     url:"https://ok.166.net/gameyw-gbox/moba/"
   },
-	onLoad: function(getPifuData) {
+	onLoad: function() {
 		var that = this;
 		wx.request({
 			// url: 'https://comp-sync.webapp.163.com/g78_hero/free_convey?callback=jQuery111308366921136572285_1540169781437&_=1540169781439',
@@ -37,31 +38,22 @@ Page({
 			},
 			responseType: "text",
 			success: function(res) {
-
-				
 				var pifuJson = unicodeToJson(res.data);
 				that.setData({
 					piFuInfo: pifuJson.result,
-				});
-				// console.log(that.data.piFuInfo[0].comment);
+        });
         that.requestShiShenData();
-		}
+      }
 	});
   },
   requestShiShenData:function(){
     var that = this;
     wx.request({
-    	/**
-    	*  获取式神录中图片的接口
-    	* var url = "https://ok.166.net/gameyw-gbox/moba/"; 
-    	*/
+      // 获取所有式神数据的接口
       url: 'https://comp-sync.webapp.163.com/g78_hero/free_convey?callback=jQuery111308366921136572285_1540169781437&_=1540169781439',
     	success: function(res) {
-        console.log(res);
     		var shishenluJson = unicodeToJson(res.data);
-        var url = "https://ok.166.net/gameyw-gbox/moba/";
         var arr = [];
-        var str = "";
         var trueJson = shishenluJson.data;
         for(var i in trueJson){
           /**
@@ -71,9 +63,14 @@ Page({
           arr.push(trueJson[i]);
         }
         that.setData({
-          shiShenLuInfo:arr
+          shiShenLuInfo:arr,
+          currentPifuUrl:that.data.piFuInfo[0]['imageUrl'],
+          currentTit:that.data.piFuInfo[0]['title'],
+          currentDesc:that.data.piFuInfo[0]['comment'],
+          currentId:that.data.piFuInfo[0]['id']
         });
-    	}
+        console.log(that.data.piFuInfo);
+      }
     });
   },
 	switchContent: function(e) {
@@ -84,13 +81,6 @@ Page({
       currentDesc:e.currentTarget.dataset.desc
 		});
 	},
-	// unicodeToJson: function(obj) {
-	// 	var start = obj.indexOf("{");
-	// 	var end = obj.lastIndexOf("}") + 1;
-	// 	var string = obj.slice(start, end);
-	// 	var json = JSON.parse(string);
-	// 	return json;
-	// },
   tabTap:function(e){
     // 点击切换选项卡
     this.setData({
