a6af44c3692879d59195e868da5df3688d2a9216
April-BOY <2111965204@qq.com>
2018-12-10 14:51:24 +0800

    feat: add skeleton screen to optimize user experience

--- pages/rank/rank.js
+++ pages/rank/rank.js
@@ -1,7 +1,15 @@
-// pages/rank/rank.js
 import {unicodeToJson,toggleRate} from '../../utils/util.js';
 Page({
   data: {
+	currentIndex:0,
+	url:"https://ok.166.net/gameyw-gbox/moba/",
+	rankData:[
+		'https://ok.166.net/gameyw-gbox/moba/gui/res/head_circle/1071.png',
+		"酒吞童子",
+		"忍",
+		"89.99%",
+		"58.54%"
+	],
     tab:[
     	{
     		name:"全",
@@ -25,38 +33,37 @@ Page({
     		name:"祝"
     	}
     ],
-	currentIndex:0,
-	url:"https://ok.166.net/gameyw-gbox/moba/",
-	rankData:[],
 	isUpRate:false,
-	isPickRate:false
+	isPickRate:false,
+	showSkeleton:true
   },
 
   onLoad: function (options) {
-			var that = this;
-			wx.request({
-				// 获取式神圆头像、式神名称和式神ID的接口
-				url: 'https://comp-sync.webapp.163.com/g78_hero/free_convey?callback=jQuery111308366921136572285_1540169781437&_=1540169781439',
-				success: function(res) {
-					var shiShenJson = unicodeToJson(res.data);
-					var arr = [];
-					var trueJson = shiShenJson.data;
-					// console.log(trueJson);
-					for(var i in trueJson){
-						arr.push(trueJson[i]);
-					}
-					// console.log(arr)
-					that.setData({
-						shiShenInfo:arr
-					});
-					
-					
-				},
-				complete:function(){
-					that.rate();
-				}
-
+	var that = this;
+    setTimeout(()=>{
+      that.setData({
+        showSkeleton:false
+      });
+    },1000);
+	wx.request({
+		// 获取式神圆头像、式神名称和式神ID的接口
+		url: 'https://comp-sync.webapp.163.com/g78_hero/free_convey?callback=jQuery111308366921136572285_1540169781437&_=1540169781439',
+		success: function(res) {
+			var shiShenJson = unicodeToJson(res.data);
+			var arr = [];
+			var trueJson = shiShenJson.data;
+			for(var i in trueJson){
+				arr.push(trueJson[i]);
+			}
+			that.setData({
+				shiShenInfo:arr
 			});
+		},
+		complete:function(){
+			that.rate();
+		}
+
+	});
 	},
 	rate:function(){
 		var that = this;
@@ -65,7 +72,6 @@ Page({
 			url: 'http://h5api.chatbot.nie.163.com/aitasks/tplcontext.lua?mid=g78_n3182_zhushou&key=0&act=get',
 			success: function(res){
 				var strJson = res.data.data;
-				// console.log(res.data.data);
 				var rawJson = JSON.parse(strJson);
 				var rateJson = rawJson.data;
 				var rateArr =[];
@@ -76,7 +82,6 @@ Page({
 						pickrate:rateJson[key][2]['pickrate']
 					});
 				}
-				// console.log(rateArr);
 				that.setData({
 					rateInfo:rateArr
 				});
@@ -93,7 +98,7 @@ Page({
 			for(var j = 0;j < this.data.rateInfo.length;j++){
 				if(this.data.shiShenInfo[i]['式神ID']==this.data.rateInfo[j]['id']){
 					arr.push({
-						head_circle:this.data.shiShenInfo[i]['式神圆头像'],
+						head_circle:that.data.url+this.data.shiShenInfo[i]['式神圆头像'],
 						name:this.data.shiShenInfo[i]['式神名称'],
 						role:this.data.shiShenInfo[i]['式神定位'],
 						winrate:this.data.rateInfo[j]['winrate'],
